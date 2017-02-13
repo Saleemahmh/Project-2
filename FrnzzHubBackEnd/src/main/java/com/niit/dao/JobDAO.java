c8df166857b3dfeb1a465a968747843dd0fcf85a
Saleemahmh <ssaleemah24@gmail.com>
2017-02-13 21:53:09 +0530

    Third Commit

---
+++ FrnzzHubBackEnd/src/main/java/com/niit/dao/JobDAO.java
@@ -0,0 +1,15 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Job;
+
+public interface JobDAO {
+
+	void postAJob(Job job);
+	List<Job> getAllJobs();
+	Job getJobDetail(int jobId);
+
+}
+
+
